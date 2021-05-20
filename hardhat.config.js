0505f6fd8b4b6216b9b73158b71ba29d71d20bc5
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-05-20 11:48:53 +0900

    add accounts task

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,15 @@ require("@nomiclabs/hardhat-waffle");
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 module.exports = {
   solidity: "0.6.12",
 };
