c319c2a66e4487c959fdbb38670d6b8dcadae6e2
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-05-22 15:57:33 +0900

    add workflows

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -7,27 +7,33 @@ const hre = require("hardhat");
 const ethers = hre.ethers;
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile 
-  // manually to make sure everything is compiled
-  await hre.run('compile');
+    // Hardhat always runs the compile task when running scripts with its command
+    // line interface.
+    //
+    // If this script is run directly using `node` you may want to call compile 
+    // manually to make sure everything is compiled
+    await hre.run('compile');
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+    // We get the contract to deploy
+    const Greeter = await hre.ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, Hardhat!");
+    const message = await greeter.greet();
 
-  await greeter.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+    await greeter.setGreeting("Hola, mundo!");
+    changed_message = await greeter.greet();
+    console.log({message, changed_message})
+
+    await greeter.deployed();
+
+    console.log("Greeter deployed to:", greeter.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
-  .then(() => process.exit(0))
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
