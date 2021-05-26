29b6361894e26e0a0ea59e5edb46f37c841c4cb1
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-05-26 11:09:57 +0900

    add aave.js

---
+++ contracts/Deposit.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+import {ILendingPool} from "@aave/protocol-v2/contracts/interfaces/ILendingPool.sol";
+
+contract Misc {
+
+  function deposit(address pool, address token, address user, uint256 amount) public {
+    ILendingPool(pool).deposit(token, amount, user, 0);
+  }
+}
--- scripts/deposit_eth.ts
+++ scripts/deposit_eth.ts
@@ -1,15 +1,23 @@
 import { run, ethers } from "hardhat";
+import { v1, v2 } from '@aave/protocol-js';
+
+let userAddress = "0x2751b3Be4B2CA09cAA3691F11A008e1Fd30F038D"
+// let userSummary = v2.formatUserSummaryData(poolReservesData, rawUserReserves, userAddress.toLowerCase(), Math.floor(Date.now() / 1000))
 
 async function main() {
-    const provider = await ethers.provider;
+    const provider = new ethers.providers.JsonRpcProvider(process.env.ALCHEMY_KOVAN_RPC_URL);
     const signer = provider.getSigner()
     console.log({
-        signer
+        signer, provider
     })
-    // const balance = await provider.getBalance("ethers.eth")
-    // console.log(ethers.utils.formatEther(balance))
+
+    console.log(
+        await provider.getBlockNumber()
+    )
 
 }
+
+// npx hardhat run --network kovan scripts/deposit_eth.ts
 
 main()
     .then(() => process.exit(0))
