51061710828b17c730353e417d8498d0e620c870
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-06-03 11:38:00 +0900

    add a address button

--- packages/app/src/components/WalletModal/index.tsx
+++ packages/app/src/components/WalletModal/index.tsx
@@ -1,30 +1,36 @@
 import useWeb3Modal from '../../hooks/useWeb3Modal';
 import { Button } from "@chakra-ui/react"
+import { useState } from 'react';
 
 export default function WalletModal() {
 
     const { provider, loadWeb3Modal, logoutOfWeb3Modal } = useWeb3Modal();
-    console.log({ provider })
-    if (provider) {
-        const blocknum = provider.getBlockNumber()
-
-        const balance = provider.getBalance("ethers.eth")
-        console.log({
-            blocknum, balance
-        })
-
+    const [address, setAddress] = useState("");
+    const getAddress = async () => {
+        if (!provider) return;
+        const signer = provider.getSigner();
+        setAddress(await signer.getAddress())
     }
+
+    getAddress()
+    console.log({ address })
+
     return (
-        <Button
-            onClick={() => {
-                if (!provider) {
-                    loadWeb3Modal();
-                } else {
-                    logoutOfWeb3Modal();
-                }
-            }}
-        >
-            {!provider ? "Connect Wallet" : "Disconnect Wallet"}
-        </Button>
+        <>
+            <Button
+                onClick={() => {
+                    if (!provider) {
+                        loadWeb3Modal();
+                    } else {
+                        logoutOfWeb3Modal();
+                    }
+                }}
+            >
+                {!provider ? "Connect Wallet" : "Disconnect Wallet"}
+            </Button>
+            <Button>
+                {address}
+            </Button>
+        </>
     )
 }
