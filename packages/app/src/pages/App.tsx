28a368af46223f9506402aecb531643882ab04f5
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-06-05 14:20:28 +0900

    delete unused hooks

--- packages/app/src/pages/App.tsx
+++ packages/app/src/pages/App.tsx
@@ -1,21 +1,13 @@
 import React, { useState } from 'react';
-import { BigNumber, Contract, providers } from 'ethers';
 import logo from '../assets/images/ethereumLogo.png';
 import WalletModal from '../components/WalletModal';
-import useContractLoader from '../hooks/useContractLoader';
-import useContractReader from '../hooks/useContractReader';
 import useWeb3Modal from '../hooks/useWeb3Modal';
-// import useWeb3Modal from '../hooks/useWeb3Modal';
 import './App.css';
-// import useGetAddress from '../hooks/useGetAddress';
 import Main from './Main';
-import { useUserAddress } from 'eth-hooks';
 
 function App() {
 
   const { provider, loadWeb3Modal, logoutOfWeb3Modal } = useWeb3Modal();
-  // const signer = provider?.getSigner();
-  // const address = useUserAddress(provider);
 
   // const readContracts = useContractLoader(provider);
   // const greet = useContractReader({
--- packages/app/src/pages/Main/index.tsx
+++ packages/app/src/pages/Main/index.tsx
@@ -3,27 +3,29 @@ import { Box } from '@chakra-ui/layout';
 import { providers } from 'ethers';
 import { useBalance, useUserAddress } from "eth-hooks";
 import { Text } from "@chakra-ui/react"
-import { formatEther, parseEther } from "@ethersproject/units";
+import { formatEther } from "@ethersproject/units";
+import { Input } from "@chakra-ui/react"
 
 interface indexProps {
-provider: providers.Web3Provider;
+    provider: providers.Web3Provider;
 }
 
-export default function Main ({
+export default function Main({
     provider
 }: indexProps) {
     const address = useUserAddress(provider)
-   const balance = useBalance(provider, address) 
-const readable = formatEther(balance)
-   console.log({
-       address, balance
-   })
-   console.log({readable})
-        return (
-            <Box>
-                <Text>
-address: {address}
-                </Text>
-            </Box>
-        );
+    const balance = useBalance(provider, address)
+    const readable = formatEther(balance)
+    console.log({
+        address, balance
+    })
+    console.log({ readable })
+    return (
+        <Box>
+            <Text>
+                address: {address}
+            </Text>
+            <Input placeholder="Greeter" />
+        </Box>
+    );
 }
