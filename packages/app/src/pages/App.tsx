97ddc83e40379877416a78c3e2c85325b4dcd739
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-06-05 19:21:39 +0900

    add theme  :lipstick:

--- packages/app/src/pages/App.tsx
+++ packages/app/src/pages/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import WalletModal from '../components/WalletModal';
 import useWeb3Modal from '../hooks/useWeb3Modal';
 import Main from './Main';
@@ -7,21 +7,13 @@ function App() {
 
   const { provider, loadWeb3Modal, logoutOfWeb3Modal } = useWeb3Modal();
 
-  // const readContracts = useContractLoader(provider);
-  // const greet = useContractReader({
-  //   contracts: readContracts, contractName: "Greeter", functionName: "greet"
-  // }
-  // );
-
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
         <WalletModal provider={provider} loadWeb3Modal={loadWeb3Modal} logoutOfWeb3Modal={logoutOfWeb3Modal} />
         {
           provider && 
           <Main provider={provider} />
         }
-      </header>
     </div>
   );
 }
