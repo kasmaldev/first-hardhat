2d78d1fbd871d1b7d31ab8c7458c376c664a4403
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-06-06 12:59:03 +0900

    useContractReader

--- packages/app/src/pages/Main/index.tsx
+++ packages/app/src/pages/Main/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Box, Center, Heading, Stack } from '@chakra-ui/layout';
 import { Contract, providers } from 'ethers';
-import { useBalance, usePoller, useUserAddress } from "eth-hooks";
+import { useBalance, useContractReader, useUserAddress } from "eth-hooks";
 import { Text } from "@chakra-ui/react"
 import { formatEther } from "@ethersproject/units";
 import { Input } from "@chakra-ui/react"
@@ -9,66 +9,70 @@ import { Button } from '@chakra-ui/button';
 import GreeterABI from '../../abis/Greeter.json'
 
 interface indexProps {
-    provider: providers.Web3Provider;
+  provider: providers.Web3Provider;
 }
 
 export default function Main({
-    provider
+  provider
 }: indexProps) {
-    const address = useUserAddress(provider)
-    const balance = useBalance(provider, address)
-    const readable = formatEther(balance)
-    console.log({readable})
+  const address = useUserAddress(provider)
+  const balance = useBalance(provider, address)
+  const readable = formatEther(balance)
+  console.log({ readable })
 
-const greeterAddress = "0x772f780EA086958D9d248380FD92763000aa113E";
+  const greeterAddress = "0x772f780EA086958D9d248380FD92763000aa113E";
 
-// The Contract object
-const greeter = new Contract(greeterAddress, GreeterABI, provider);
-const signer = provider.getSigner()
+  const getNetwork = async () => {
 
-const Greeter = greeter.connect(signer);
-const changeMessage = (message: string) => {
+    const network = await provider.getNetwork()
+    const name = network.name 
+    console.log({
+      network, name
+    })
+  }
+
+  getNetwork()
+
+  // The Contract object
+  const greeter = new Contract(greeterAddress, GreeterABI, provider);
+
+
+  const greetMessage = useContractReader(greeter, "greet");
+  const signer = provider.getSigner()
+
+  const Greeter = greeter.connect(signer);
+  const changeMessage = (message: string) => {
     Greeter.setGreeting(message)
-}
+  }
   const [value, setValue] = useState("")
   const handleChange = (event: any) => setValue(event.target.value)
-const handleClick = () => {
+  const handleClick = () => {
     changeMessage(value);
-}
+  }
 
-const [hello, setHello] = useState("")
+  return (
+    <Box>
+      <Center>
+        <Stack spacing={8}>
 
-const showMessage = async () => {
-  const response = await Greeter.greet()
-  setHello(response)
-}
-usePoller(showMessage, 3000)
-    return (
-        <Box>
-          <Center>
-            <Stack spacing={8}>
+          <Box p={5} shadow="md" borderWidth="1px" >
+            <Heading fontSize="xl">{address}</Heading>
+            <Text mt={4}>greet: {greetMessage}</Text>
+            {/* <Text mt={4}>network name: {network.name}</Text> */}
+          </Box>
 
-    <Box p={5} shadow="md" borderWidth="1px" >
-      <Heading fontSize="xl">{address}</Heading>
-      <Text mt={4}>{hello}</Text>
+          <Box p={5} shadow="md" borderWidth="1px" >
+            <Heading fontSize="xl">{address}</Heading>
+          </Box>
+        </Stack>
+        <form>
+          <Input value={value}
+            onChange={handleChange}
+            placeholder="Greeter" />
+          <Button onClick={handleClick}>
+            change</Button>
+        </form>
+      </Center>
     </Box>
-
-    <Box p={5} shadow="md" borderWidth="1px" >
-      <Heading fontSize="xl">{address}</Heading>
-      <Text mt={4}>{hello}</Text>
-    </Box>
-            </Stack>
-            <form>
-            <Input value={value}
-        onChange={handleChange}
-        placeholder="Greeter" />
-            <Button onClick={handleClick}>
-                change</Button> 
-            </form>
-            <Text>
-              {hello}
-            </Text>
-        </Center>
-        </Box>
-    );
+  );
 }
